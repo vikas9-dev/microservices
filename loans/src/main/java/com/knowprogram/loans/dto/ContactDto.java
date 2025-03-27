8c2e540d21c58e7d874a2b4962c8d3b715bf5494
vikas9dev <191975771+vikas9-dev@users.noreply.github.com>
2025-03-27 17:09:47 +0530

    Spring Cloud Config Project Added

---
+++ loans/src/main/java/com/knowprogram/loans/dto/ContactDto.java
@@ -0,0 +1,11 @@
+package com.knowprogram.loans.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cards")
+public record ContactDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+
+}
