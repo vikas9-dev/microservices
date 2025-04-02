98727165a332bbacaa289ff322a6739fec84cad9
vikas9dev <vikas9dev>
2025-04-02 09:39:18 +0530

    Docker Compose file created

--- configserver/src/main/java/com/knowprogram/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/knowprogram/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.knowprogram.config_server;
+package com.knowprogram.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
